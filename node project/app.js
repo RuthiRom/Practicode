4079c948f526705cd03ef8e677084c7fcc3b568a
unknown <you@example.com>
2023-05-15 19:02:10 +0300

    commit9

--- node project/app.js
+++
@@ -1,8 +0,0 @@
-import api from 'api'
-
-const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
-
-sdk.auth('rnd_7B1ekAL7b9B7f2S0PJOtv8Jl4n0K');
-sdk.getServices({limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
---
+++ node project/index.js
@@ -0,0 +1,78 @@
+import express from "express";
+
+app.listen(port,()=>{
+
+const app = express()
+const api = [
+  {
+    "cursor": "qPjs9UlzbNVqbWlrNzI4c2Q2aWVtYWhn",
+    "service": {
+      "id": "srv-chgmjmik728sd6iemahg",
+      "autoDeploy": "yes",
+      "branch": "main",
+      "buildFilter": null,
+      "createdAt": "2023-05-14T23:08:11.047632Z",
+      "name": "ToDoApi",
+      "notifyOnFail": "default",
+      "ownerId": "usr-chgem53hp8u065u1klc0",
+      "repo": "https://github.com/RuthiRom/Practicode",
+      "rootDir": "./ToDoApi",
+      "slug": "todoapi3-kb3v",
+      "suspended": "not_suspended",
+      "suspenders": [],
+      "type": "web_service",
+      "updatedAt": "2023-05-15T13:13:16.213903Z",
+      "serviceDetails": {
+        "disk": null,
+        "env": "docker",
+        "envSpecificDetails": {
+          "dockerCommand": "",
+          "dockerContext": ".",
+          "dockerfilePath": "Dockerfile"
+        },
+        "healthCheckPath": "",
+        "numInstances": 1,
+        "openPorts": null,
+        "parentServer": null,
+        "plan": "free",
+        "pullRequestPreviewsEnabled": "no",
+        "region": "singapore",
+        "url": "https://todoapi3-kb3v.onrender.com"
+      }
+    }
+  },
+  {
+    "cursor": "WODUlIXPW8w2bTY3YXZqYmJqcG1uZDJn",
+    "service": {
+      "id": "srv-chgh6m67avjbbjpmnd2g",
+      "autoDeploy": "yes",
+      "branch": "main",
+      "buildFilter": null,
+      "createdAt": "2023-05-14T16:59:04.597982Z",
+      "name": "ToDoListReact-master",
+      "notifyOnFail": "default",
+      "ownerId": "usr-chgem53hp8u065u1klc0",
+      "repo": "https://github.com/RuthiRom/Practicode",
+      "rootDir": "./ToDoListReact-master",
+      "slug": "todolistreact-master",
+      "suspended": "not_suspended",
+      "suspenders": [],
+      "type": "static_site",
+      "updatedAt": "2023-05-15T13:13:29.176834Z",
+      "serviceDetails": {
+        "buildCommand": "npm run build",
+        "parentServer": null,
+        "publishPath": "build",
+        "pullRequestPreviewsEnabled": "no",
+        "url": "https://todolistreact-master.onrender.com"
+      }
+    }
+  }
+]
+const port=3000
+console.log("nice!!!!!!!")
+app.get('/', (req, res) => {
+  res.send(api)
+})
+console.log("app is running!")
+})
